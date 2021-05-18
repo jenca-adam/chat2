bba33602738c9da3ee88657f613c661b5c18b723
Adam Jenča <jenca.adam@gmail.com>
2021-05-18 20:10:09 +0200

    sc

--- server.py
+++ server.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python
 
-import flask,pickle,time,os,models
+import flask,time,os,models
 from flask_cors import CORS, cross_origin
 from werkzeug.utils import secure_filename
 app=flask.Flask(__name__)
 app.secret_key=b'[gsk37837/eshjn\x09\x09ao""\x01\xff\xab\xdd ;a'
-app.config['UPLOAD_FOLDER']=os.path.join(os.getcwd(),'uploads')
 cors=CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+
 @app.route('/')
 def index():
     return flask.render_template('index.html')
+
 @app.route('/auth/',methods=['GET','POST'])
 def auth():
     if flask.request.method=='POST':
@@ -23,42 +24,42 @@ def auth():
             return flask.render_template('index.html',error='Asi si zadal zly kod!')
             
     return flask.abort(401)
+
 @app.route('/ukaz/',methods=['GET','POST'])
 def ukaz():
-    try:
-        if not flask.session['allwd']:
-            return flask.redirect('/')
-    except KeyError:
+
+    if not flask.session.get('allwd',False):
         return flask.redirect('/')
     if flask.request.method=='POST':
         flask.session['user']=flask.request.form['nick']
         return flask.redirect('/forum/')
     
     return flask.render_template('ukaz.html')
+
 @app.route('/forum/' , methods=['GET','POST'])
 def forum():
-    try:
-        curuser=flask.session['user']
-    except KeyError:
+    if 'user' not in flask.session:
         return flask.redirect('/')
-    prispevky=models.all_p()        
+    curuser=flask.session['user']
     return flask.render_template('forum.html',usrname=curuser)
+
 @app.route('/forum/table/')
 @cross_origin()
-def rtable():
-    try:
-        curuser=flask.session['user']
-    except KeyError:
+def table():
+    if 'user' not in flask.session:
         return flask.redirect('/')
+  
+    curuser=flask.session['user']
     prispevky=models.all_p() 
     return flask.render_template('table.html',prispevky=prispevky)
+
 @app.route('/forum/add/',methods=['POST','GET'])
 def pridaj():
     if flask.request.method=='POST':
-        try:
-            curuser=flask.session['user']
-        except KeyError:
+        if 'user' not in flask.session:
             return flask.redirect('/')
+
+        curuser=flask.session['user']
         form=flask.request.form
         text=form['prispevok']
         timem=time.ctime(time.time())
@@ -66,10 +67,12 @@ def pridaj():
         models.Prispevok(author,timem,text)
         return flask.redirect('/forum/')
     return flask.redirect('/')
+
 @app.route('/logout/')
 def odhlas():
     del flask.session['user']
     return flask.redirect('/ukaz/')
+
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return flask.send_from_directory(app.config['UPLOAD_FOLDER'],
